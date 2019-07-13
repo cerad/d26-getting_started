31d054db559cea3c9c76641fff5f1da0e8b734a0
ahundiak <ahundiak@gmail.com>
2019-07-13 12:46:58 -0500

    Added xml file

--- bootstrap.php
+++ bootstrap.php
@@ -8,9 +8,9 @@ require_once "vendor/autoload.php";
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+//$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
 // or if you prefer yaml or XML
-//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 
 // database configuration parameters
